5abba7c2ce39211e5921e7f4ff3f16276f5e506a
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-06-14 08:36:40 +0300

    commit tree 30d3e6461fbdf22083af7dda4541b08959443072

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let destination = 42;
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - destination);
+}
+
+function distanceFromHqInFeet(location) {
+    let blocks = distanceFromHqInBlocks(location);
+    return blocks * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(end - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (2 * (distance - 400)) / 100;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    }else {
+        return "cannot travel that far"
+    }
+}
